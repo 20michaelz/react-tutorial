129c88038c964c13c4e02a282e1533497be165ca
Michael Zhao <michaelzhao@Michaels-MacBook-Air-2.local>
2021-05-28 11:32:23 -0700

    Fixed errors with import preventing compilation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import Table from './Table'
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello, React!</h1>
+      <div className="container">
+        <Table />
       </div>
     )
   }
 }
 
-export default App;
+export default App
