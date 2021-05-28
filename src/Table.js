bd3d27e057203bec6f119696e9475efc98867b55
Michael Zhao <michaelzhao@michaels-air-2.lan>
2021-05-28 15:53:24 -0700

    Set up Delete Buttons for Table, Setting up Form to input data

--- src/Table.js
+++ src/Table.js
@@ -11,39 +11,30 @@ const TableHeader = () => {
   )
  }
 
-const TableBody = () => { 
-  return (
-    <tbody>
-      <tr>
-        <td>Charlie</td>
-        <td>Janitor</td>
-      </tr>
-      <tr>
-        <td>Mac</td>
-        <td>Bouncer</td>
-      </tr>
-      <tr>
-        <td>Dee</td>
-        <td>Aspiring actress</td>
-      </tr>
-      <tr>
-        <td>Dennis</td>
-        <td>Bartender</td>
-      </tr>
-    </tbody>
-  )
- }
-
-
-class Table extends Component {
-  render() {
+const TableBody = (props) => { 
+  const rows = props.characterData.map((row, index) => {
     return (
-      <table>
-        <TableHeader />
-        <TableBody />
-      </table>
+      <tr key = {index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
+      </tr>
     )
-  }
+  })
+  return <tbody>{rows}</tbody>
+}
+
+const Table = (props) => {
+  const {characterData, removeCharacter} = props
+
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData = {characterData} removeCharacter={removeCharacter} />
+    </table>
+  )
 }
 
 export default Table
