fea8ef72b5c83480dd45bb9d42195ca9706eca74
Michael Zhao <michaelzhao@Michaels-MacBook-Air-2.local>
2021-05-31 09:01:57 -0700

    Implemented Form

--- src/Form.js
+++ src/Form.js
@@ -15,4 +15,41 @@ class Form extends Component {
             [name]: value,
         })
     }
+
+    submitForm = () => {
+        this.props.handleSubmit(this.state)
+        this.setState(this.initialState)
+    }
+
+    render() {
+        const {name, job} = this.state;
+
+        return(
+            <form>
+                <label htmlFor="name">Name</label>
+                <input
+                    type = "text"
+                    name = "name"
+                    id = "name"
+                    value = {name}
+                    onChange = {this.handleChange} />
+                
+                <label htmlFor="job">Job</label>
+                <input
+                    type = "text"
+                    name = "job"
+                    id = "job"
+                    value = {job}
+                    onChange = {this.handleChange} />
+                <input 
+                    type = "button"
+                    value = "Submit"
+                    onClick = {this.submitForm} />
+            </form>
+        );
+    }
 }
+
+export default Form;
+
+    
